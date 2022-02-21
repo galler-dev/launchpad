fb413364aa7628c3668fc3058adad48bb8c52796
macrov.g <macrov.g@binance.com>
2022-02-21 14:05:43 +0800

    refactor

--- contracts/Launchpad.sol
+++ contracts/Launchpad.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ILaunchpadNFT.sol";
 
-contract Launchpad is Ownable {
+contract Launchpad is Ownable, ReentrancyGuard {
 
     event AddCampaign(address contractAddress, address payeeAddress, uint256 price, uint256 maxSupply,
                         uint256 listingTime, uint256 expirationTime, uint256 maxBatch, uint256 maxPerAddress);
@@ -23,21 +24,21 @@ contract Launchpad is Ownable {
         uint256 maxPerAddress;
     }
 
-    mapping(address => Campaign) public campaigns;
-    mapping(address => mapping(address => uint256)) public mintRecords;
+    mapping(address => Campaign) private _campaigns;
+    mapping(address => mapping(address => uint256)) private _mintPerAddress;
 
-    function mint(address contractAddress, uint256 batchSize) payable external {
+    function mint(address contractAddress, uint256 batchSize) external payable nonReentrant {
         // basic check
         require(contractAddress != address(0), "contract address can not be empty");
         require(batchSize > 0, "batchSize must greater than 0");
-        require(campaigns[contractAddress].contractAddress != address(0), "contract not register");
+        require(_campaigns[contractAddress].contractAddress != address(0), "contract not register");
 
         // activity check
-        Campaign memory campaign = campaigns[contractAddress];
+        Campaign memory campaign = _campaigns[contractAddress];
         require(batchSize <= campaign.maxBatch, "reach max batch size");
         require(block.timestamp >= campaign.listingTime, "activity not start");
         require(block.timestamp < campaign.expirationTime, "activity ended");
-        require(mintRecords[contractAddress][msg.sender] + batchSize <= campaign.maxPerAddress, "reach max per address limit");
+        require(_mintPerAddress[contractAddress][msg.sender] + batchSize <= campaign.maxPerAddress, "reach max per address limit");
         // NFT contract must impl ERC721Enumerable to have this totalSupply method
         uint256 currentSupply = ILaunchpadNFT(contractAddress).getLaunchpadSupply();
         require(currentSupply + batchSize <= campaign.maxSupply, "reach campaign max supply");
@@ -45,7 +46,7 @@ contract Launchpad is Ownable {
         require(msg.value >= totalPrice, "value not enough");
 
         // update record
-        mintRecords[contractAddress][msg.sender] = mintRecords[contractAddress][msg.sender] + batchSize;
+        _mintPerAddress[contractAddress][msg.sender] = _mintPerAddress[contractAddress][msg.sender] + batchSize;
 
         // transfer token and mint
         payable(campaign.payeeAddress).transfer(totalPrice);
@@ -53,46 +54,54 @@ contract Launchpad is Ownable {
 
         emit Mint(campaign.contractAddress, campaign.payeeAddress, batchSize, campaign.price);
         // return
-        payable(_msgSender()).transfer(msg.value - totalPrice);
+        uint256 valueLeft = msg.value - totalPrice;
+        if (valueLeft > 0) {
+            payable(_msgSender()).transfer(valueLeft);
+        }
     }
 
     function getMintPerAddress(address contractAddress, address userAddress) view external returns (uint256) {
-        require(campaigns[contractAddress].contractAddress != address(0), "contract address invalid");
+        require(_campaigns[contractAddress].contractAddress != address(0), "contract address invalid");
         require(userAddress != address(0), "user address invalid");
-
-        return mintRecords[contractAddress][userAddress];
+        return _mintPerAddress[contractAddress][userAddress];
     }
 
-    function addCampaign(address contractAddress_, address payeeAddress_, uint256 price_, uint256 maxSupply_,
+    function getMaxSupply(address contractAddress) view external returns (uint256) {
+        return ILaunchpadNFT(contractAddress).getMaxLaunchpadSupply();
+    }
+
+    function addCampaign(address contractAddress_, address payeeAddress_, uint256 price_,
         uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_, uint256 maxPerAddress_) external onlyOwner {
         require(contractAddress_ != address(0), "contract address can not be empty");
-        require(campaigns[contractAddress_].contractAddress == address(0), "contract address already exist");
+        require(_campaigns[contractAddress_].contractAddress == address(0), "contract address already exist");
         require(payeeAddress_ != address(0), "payee address can not be empty");
-        require(maxSupply_ > 0, "max supply can not be 0");
         require(maxBatch_ > 0, "max batch invalid");
         require(maxPerAddress_ > 0, "max per address can not be 0");
+        uint256 maxSupply_ = ILaunchpadNFT(contractAddress_).getMaxLaunchpadSupply();
+        require(maxSupply_ > 0, "max supply can not be 0");
         emit AddCampaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
             expirationTime_, maxBatch_, maxPerAddress_);
-        campaigns[contractAddress_] = Campaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
+        _campaigns[contractAddress_] = Campaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
                                                 expirationTime_, maxBatch_, maxPerAddress_);
     }
 
-    function updateCampaign(address contractAddress_, address payeeAddress_, uint256 price_, uint256 maxSupply_,
+    function updateCampaign(address contractAddress_, address payeeAddress_, uint256 price_,
         uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_, uint256 maxPerAddress_) external onlyOwner {
         require(contractAddress_ != address(0), "contract address can not be empty");
-        require(campaigns[contractAddress_].contractAddress != address(0), "contract address not exist");
+        require(_campaigns[contractAddress_].contractAddress != address(0), "contract address not exist");
         require(payeeAddress_ != address(0), "payee address can not be empty");
-        require(maxSupply_ > 0, "max supply can not be 0");
         require(maxBatch_ > 0, "max batch invalid");
         require(maxPerAddress_ > 0, "max per address can not be 0");
+        uint256 maxSupply_ = ILaunchpadNFT(contractAddress_).getMaxLaunchpadSupply();
+        require(maxSupply_ > 0, "max supply can not be 0");
         emit UpdateCampaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
                             expirationTime_, maxBatch_, maxPerAddress_);
-        campaigns[contractAddress_] = Campaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
+        _campaigns[contractAddress_] = Campaign(contractAddress_, payeeAddress_, price_, maxSupply_, listingTime_,
                                                 expirationTime_, maxBatch_, maxPerAddress_);
     }
 
     function getCampaign(address contractAddress) view external returns (address, address, uint256, uint256, uint256, uint256, uint256, uint256) {
-        Campaign memory a = campaigns[contractAddress];
+        Campaign memory a = _campaigns[contractAddress];
         return (a.contractAddress, a.payeeAddress, a.price, a.maxSupply, a.listingTime, a.expirationTime, a.maxBatch, a.maxPerAddress);
     }
 }
--- test/LaunchpadTest.js
+++ test/LaunchpadTest.js
@@ -9,9 +9,11 @@ contract("Launchpad", (accounts) => {
         let launchpad = await Launchpad.new();
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addActivity(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
-        truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 1, 2));
-        truffleAssert.reverts(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 1, 2), "contract address already exist");
-        truffleAssert.passes(launchpad.updateCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 2, 2));
+        truffleAssert.reverts(launchpad.addCampaign("0x0000000000000000000000000000000000000000", accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2), "contract address can not be empty");
+        truffleAssert.reverts(launchpad.addCampaign(nft.address, "0x0000000000000000000000000000000000000000", web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2), "payee address can not be empty");
+        truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2));
+        truffleAssert.reverts(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2), "contract address already exist");
+        truffleAssert.passes(launchpad.updateCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 2, 2));
     });
 
     it("mint", async () => {
@@ -20,7 +22,7 @@ contract("Launchpad", (accounts) => {
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addCampaign(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
         await truffleAssert.reverts(launchpad.mint(nft.address, 1), "contract not register");
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 1, 2));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2));
 
         // let res = await launchpad.getActivity(nft.address);
         //
@@ -29,6 +31,9 @@ contract("Launchpad", (accounts) => {
 
         let mintPerAddress = await launchpad.getMintPerAddress(nft.address, accounts[0]);
         assert.equal(mintPerAddress.toNumber(), 1);
+
+        let maxSupply = await launchpad.getMaxSupply(nft.address);
+        assert.equal(maxSupply.toNumber(), 10);
     });
 
     it("mint per address", async () => {
@@ -37,7 +42,7 @@ contract("Launchpad", (accounts) => {
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addCampaign(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
         await truffleAssert.reverts(launchpad.mint(nft.address, 1), "contract not register");
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 3, 2));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 3, 2));
 
         await truffleAssert.reverts(launchpad.mint(nft.address, 3, {value : web3.utils.toWei("0.003", "ether")}), "reach max per address limit");
         await truffleAssert.passes(launchpad.mint(nft.address, 2, {value : web3.utils.toWei("0.002", "ether")}));
@@ -50,7 +55,7 @@ contract("Launchpad", (accounts) => {
         let launchpad = await Launchpad.new();
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addActivity(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 1, 2));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 1, 2));
         await truffleAssert.reverts(launchpad.mint(nft.address, 2, {value : web3.utils.toWei("0.002", "ether")}), "reach max batch size");
     });
 
@@ -59,7 +64,7 @@ contract("Launchpad", (accounts) => {
         let launchpad = await Launchpad.new();
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addActivity(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 10, 10));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 10, 10));
         await truffleAssert.passes(launchpad.mint(nft.address, 10, {value : web3.utils.toWei("0.01", "ether")}));
         await truffleAssert.reverts(launchpad.mint(nft.address, 10, {from: accounts[1], value : web3.utils.toWei("0.01", "ether")}), "reach campaign max supply");
     });
@@ -71,7 +76,7 @@ contract("Launchpad", (accounts) => {
         // addActivity(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
         const account1BalanceBefore = await web3.eth.getBalance(accounts[1]);
 
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1744390809, 10, 10));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1744390809, 10, 10));
         await truffleAssert.passes(launchpad.mint(nft.address, 10, {value : web3.utils.toWei("0.01", "ether")}));
 
         const account1BalanceAfter = await web3.eth.getBalance(accounts[1]);
@@ -84,9 +89,9 @@ contract("Launchpad", (accounts) => {
         let launchpad = await Launchpad.new();
         let nft = await LaunchpadNFT.new("LaunchpadNFT", "LNFT", "https://metadata.com/nft/", launchpad.address, 10);
         // addActivity(address contractAddress_, uint256 price_, uint256 totalSupply_, uint256 listingTime_, uint256 expirationTime_, uint256 maxBatch_)
-        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1744390809, 1744390809, 10, 10));
+        await truffleAssert.passes(launchpad.addCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1744390809, 1744390809, 10, 10));
         await truffleAssert.reverts(launchpad.mint(nft.address, 10, {value : web3.utils.toWei("0.01", "ether")}), "activity not start");
-        await truffleAssert.passes(launchpad.updateCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 10, 1644390809, 1644390809, 10, 10));
+        await truffleAssert.passes(launchpad.updateCampaign(nft.address, accounts[1], web3.utils.toWei("0.001", "ether"), 1644390809, 1644390809, 10, 10));
         await truffleAssert.reverts(launchpad.mint(nft.address, 10, {value : web3.utils.toWei("0.01", "ether")}), "activity ended");
     });
 })
